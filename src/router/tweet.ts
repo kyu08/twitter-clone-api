e16517b2f8ef04f7f6a76adebc6514658e44311a
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-08-03 01:57:32 +0900

    add tweet/:tweetId/likes, retweets

--- src/router/tweet.ts
+++ src/router/tweet.ts
@@ -14,4 +14,18 @@ router.get('/:tweetId', (req, res) => {
   return res.send(req.params);
 });
 
+router.get('/:tweetId/likes', (req, res) => {
+  console.log('GET /tweet/:tweetId/likes called');
+  console.log(req.params);
+  const hoge = ['taro', 'jiro'];
+  return res.send(hoge);
+});
+
+router.get('/:tweetId/retweets', (req, res) => {
+  console.log('GET /tweet/:tweetId/retweets called');
+  console.log(req.params);
+  const hoge = ['taro', 'jiro'];
+  return res.send(`${hoge}RT`);
+});
+
 export default router;
