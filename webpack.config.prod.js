eb15337c9e82fc3c90193e310f6bd13cb85445f5
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-08-02 16:09:36 +0900

    add rules

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,30 +1,30 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/server.ts',     // src下に書いていくので　src/server.tsにしとく
-    target: 'node',               // Module not found: Error: Can't resolve 'fs'とかいっぱい出たら、この行書き忘れ
-    externals: [nodeExternals()], 
-    module: {
-        rules: [
-            {
-                loader: 'ts-loader',
-                test: /\.ts$/,
-                exclude: [
-                    /node_modules/
-                ],
-                options: {
-                    configFile: 'tsconfig.dev.json'
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: [ '.ts', '.js' ]
-    },
-    output: {
-        filename: 'server.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  mode: 'production',
+  entry: './src/server.ts', // src下に書いていくので　src/server.tsにしとく
+  target: 'node', // Module not found: Error: Can't resolve 'fs'とかいっぱい出たら、この行書き忘れ
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        options: {
+          configFile: 'tsconfig.dev.json',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
